fac4af477cc2ace304d2a39b563bb48cae205f9e
wesmartin17 <wes.martin17@gmail.com>
2017-03-10 15:37:09 -0500

    i

--- test.php
+++ test.php
@@ -2,13 +2,13 @@
 $host="web0.site.uottawa.ca:15432";
 $user="sgian032";
 $password="rvwf78rvwf78@23";
-$con=mysqli_connect($host,$user,$password,"foobox");
+$con=mysql_connect($host,$user,$password,"foobox");
 if($con){
 	$sql = "SELECT * FROM User_Account";
-	$result = mysqli_query($con,$sql);
+	$result = mysql_query($con,$sql);
 
 	if(mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result)){
+		while($row = mysql_fetch_assoc($result)){
 			echo "username: ".$row["password"].", password: ".$row["password"].", type: ".$row["category"]."<br>";
 		}
 	}
