de84ff272d4a33efaa2cc4abcbc5c6457bf000bc
Projudah <projudah@gmail.com>
2017-03-15 14:00:32 -0400

    Update index.php
    Changed the user

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $con_string = "host=web0.site.uottawa.ca port=15432 dbname=sgian032 user=sgian032 password=***REMOVED***";
+    $con_string = "host=web0.site.uottawa.ca port=15432 dbname=jolot104 user=jolot104 password=***REMOVED***";
     $con = pg_connect($con_string);
     $username = $_POST["username"];
     $password = $_POST["password"];
     if($con){
       $result =pg_query($dbconn4, 
-                      "set search_path = 'foobox'; select * from user_account;
+                      "set search_path = 'As'; select message from test;
                       ");
        $my = pg_fetch_row($result);
        echo print_r($my);
