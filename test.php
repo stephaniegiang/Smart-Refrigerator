87132274fac3ea890efc2711975038081e84300c
Projudah <projudah@gmail.com>
2017-03-15 14:22:21 -0400

    Update test.php
    test working

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
-$conn_string = "host=web0.site.uottawa.ca port=15432 dbname=jolot104 user=jolot104 password=***REMOVED***";
+$conn_string = "host=web0.site.uottawa.ca port=15432 dbname=sgian032 user=sgain032 password=***REMOVED***";
 $dbconn4 = pg_connect($conn_string);
 if(!$dbconn4){
 	echo "Connection failed";
 }
 
-$tab = pg_query($dbconn4, "set search_path = 'As'; Select message from test;");
+$tab = pg_query($dbconn4, "set search_path = 'foobox'; Select * from user_account;");
 if (!$tab) {
   echo "An error occurred.\n";
 }
