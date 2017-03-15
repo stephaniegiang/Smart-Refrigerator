6fb74e153d89b276c3988debb54c81990877bfb3
Projudah <projudah@gmail.com>
2017-03-15 14:19:31 -0400

    Update test.php
    testing again

--- test.php
+++ test.php
@@ -1,12 +1,14 @@
 <?php
-    $con_string = "host=web0.site.uottawa.ca port=15432 dbname=jolot104 user=jolot104 password=***REMOVED***";
-    $con = pg_connect($con_string);
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    if($con){
-      $result =pg_query($dbconn4,"set search_path = 'As'; select message from test;");
-       $my = pg_fetch_row($result);
-       echo print_r($my);
-       echo $my[0];
-     }
+$conn_string = "host=web0.site.uottawa.ca port=15432 dbname=jolot104 user=jolot104 password=***REMOVED***";
+$dbconn4 = pg_connect($conn_string);
+if(!$dbconn4){
+	echo "Connection failed";
+}
+
+$tab = pg_query($dbconn4, "set search_path = 'As'; Select message from test;");
+if (!$tab) {
+  echo "An error occurred.\n";
+}
+$row = pg_fetch_row($tab);
+echo print_r($row);
 ?>
