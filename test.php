f48c4027eb4b2f506ab35c8adcf09a7d0f3b5528
Projudah <projudah@gmail.com>
2017-03-15 14:15:45 -0400

    Update test.php
    testing

--- test.php
+++ test.php
@@ -1,27 +1,12 @@
 <?php
-$host="web0.site.uottawa.ca:15432";
-$user="sgian032";
-$password="***REMOVED***";
-$con=mysql_connect($host,$user,$password,"foobox");
-if($con){
-	$sql = "SELECT * FROM User_Account";
-	$result = mysql_query($con,$sql);
-
-	if(mysqli_num_rows($result) > 0){
-		while($row = mysql_fetch_assoc($result)){
-			echo "username: ".$row["password"].", password: ".$row["password"].", type: ".$row["category"]."<br>";
-		}
-	}
-	else{
-		echo "0 results";
-	}
-
-
-
-
-
-}
-else{
-	echo'not connected';
-}
+    $con_string = "host=web0.site.uottawa.ca port=15432 dbname=jolot104 user=jolot104 password=***REMOVED***";
+    $con = pg_connect($con_string);
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    if($con){
+      $result =pg_query($dbconn4,"set search_path = 'As'; select message from test;");
+       $my = pg_fetch_row($result);
+       echo print_r($my);
+       echo $my[0];
+     }
 ?>
