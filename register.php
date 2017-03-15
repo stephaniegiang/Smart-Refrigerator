c9c28cc3c75d7adce90abb4a2f6e40ef6604da93
Wes <wes.martin17@gmail.com>
2017-03-15 17:33:42 -0400

    fixed the registration type. now registers every field

--- register.php
+++ register.php
@@ -6,17 +6,15 @@
     $name = $_POST["name"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $role = $_POST["role"];
-    echo $role;
-/*
+    $role = $_POST['role'];
+
     if($con){
-      $submit = pg_query($con, "set search_path = 'foobox'; Insert into User_Account(Name, Username, Password, Category) values ('".$name."', '".$username."', '".$password."', '".$role."')");
+      $submit = pg_query($con, "set search_path = 'foobox'; Insert into User_Account(Name, Username, Password, Category) values ('".$name."', '".$username."', '".$password."', '".$role[0]."')");
       if($submit)
-        echo "Thanks, ".$username." You are now registered. (and eventually redirected once its implemented";
+        echo "Thanks, ".$username." You are now registered as a ".$role[0].". (and eventually redirected once its implemented";
       else
         echo "could not connect. welp";
      }
-     **/
 
   }
 ?>
