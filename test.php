9da140b5b09ee97efc12fe2afb4cc137c35cbe0e
Projudah <projudah@gmail.com>
2017-03-20 14:56:40 -0400

    commit

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
-$conn_string = "host=web0.site.uottawa.ca port=15432 dbname=sgian032 user=sgain032 password=rvwf78rvwf78@23";
+$conn_string = "host=web0.site.uottawa.ca port=15432 dbname=sgian032 user=sgian032 password=rvwf78rvwf78@23";
 $dbconn4 = pg_connect($conn_string);
 if(!$dbconn4){
 	echo "Connection failed";
 }
-
 $tab = pg_query($dbconn4, "set search_path = 'foobox'; Select * from user_account;");
 if (!$tab) {
   echo "An error occurred.\n";
