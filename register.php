a07f7cc56d74b75f21d60645542c81bd0a35baf8
Wes <wes.martin17@gmail.com>
2017-03-21 16:38:00 -0400

    fixed the user type not showing correctly

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 
     $submit = pg_query("set search_path = 'foobox'; Insert into User_Account(Name, Username, Password, Category) values ('$name', '$username', '$password', '$role[0]');");
       if($submit)
-        echo "Thanks, ".$username." You are now registered as a ".$role[0].". (and eventually redirected once its implemented";
+        echo "Thanks, ".$username." You are now registered as a ".$role.". (and eventually redirected once its implemented";
       else
         echo "could not connect. welp";
 ?>
