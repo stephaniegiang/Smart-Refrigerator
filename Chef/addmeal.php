b518dab2b6d85e6f2d46550e0f82988935077577
Projudah <projudah@gmail.com>
2017-03-27 11:13:48 -0400

    fixed add meal

--- Chef/addmeal.php
+++ Chef/addmeal.php
@@ -3,14 +3,19 @@
     <div style="margin-top:50px;margin-bottom:100px;">
     	<h2>Enter meal details</h2>
       <div style="width: 45%; background-color: white;">
+      <form action="submitmeal.php" method="Post">
         <label for="Mealname">Name</label>
-        <input type="text" name="username" required="required" id="Mealname" tabindex="1" class="form-control" placeholder="Meal Name" value="">
+        <input type="text" name="mealname" required="required" id="mealname" tabindex="1" class="form-control" placeholder="Meal Name" value="">
         <br/>
         <label for="Cuisinename">Cuisine</label>
-        <input type="text" name="username" required="required" id="Cuisinename" tabindex="1" class="form-control" placeholder="E.g Italian,Chinese.." value="">
+        <input type="text" name="cuisinename" required="required" id="cuisinename" tabindex="1" class="form-control" placeholder="E.g Italian,Chinese.." value="">
         <br/>
         <label for="Price">Price</label>
-        $<input type="number" name="username" required="required" id="Price" tabindex="1" class="form-control" placeholder="$" value="">
+        $<input type="number" name="price" required="required" id="price" tabindex="1" class="form-control" placeholder="$" value=""> <br/>
+
+        <button type ="submit" class="btn btn-primary" style="width: 50%;">Add</button>
+        </form>
+
       </div>
     </div>
     
