e9dfe526b51976807da8038f6f6fb091687d181c
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-28 17:01:49 -0400

    Fixing merge issues

--- Chef/addmeal.php
+++ Chef/addmeal.php
@@ -36,37 +36,6 @@
   </div>
   <button type ="submit" class="btn btn-primary" style="width: 25%; margin-top: 10px; ">Add</button>
 </form>
-
-<<<<<<< HEAD
-        <h2> Select your ingredients</h2>
-      <div class="col-md-5">
-        <select style="width:100%;min-height:50%;" id="sbOne" multiple="multiple">
-          <?php
-            require('../connect.php');
-            $setPath = pg_query("Set search_path='foobox';");
-            $getIngredients = pg_query("Select * from Ingredient;");
-            while($row = pg_fetch_array($getIngredients)){
-              echo '<option class="op" value="'.$row["id"]. '"">' .$row["name"].'</option>';
-            }
-          ?>
-        </select>
-        <button style="width:100%;" onclick="populateOnce('sbOne','sbTwo')"type="button" class="btn btn-default" id="left" value="<">Select Ingredient</button>
-    </div>
-    <div class="col-md-5">
-    <form action="orderingTheIngredient.php" method="post">
-        <select name="list[]" style="width:100%;min-height:50%;" id="sbTwo" multiple="multiple">
-        </select>
-        <button style="width:100%;" type="submit" class="btn btn-default" id="left" value="<">ORDER</button>
-        </form>
-        <button style="width:100%;"onclick="populateOnce('sbTwo','sbOne')"type="button" class="btn btn-default" id="left" value="<">Remove Selected</button>
-        
-    </div>
-
-        <button type ="submit" class="btn btn-primary" style="width: 50%;">Add</button>
-        </form>
-=======
->>>>>>> master
-
 </div>
 
 </html>
