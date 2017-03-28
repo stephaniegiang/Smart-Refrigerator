43a58c1a0837a13ede2adcb3ebc11585b7d597da
Projudah <projudah@gmail.com>
2017-03-28 16:39:25 -0400

    work in progress add meal

--- register.php
+++ register.php
@@ -5,9 +5,19 @@
     $password = $_POST["password"];
     $role = $_POST["role"];
 
-    $submit = pg_query("set search_path = 'foobox'; Insert into User_Account(Name, Username, Password, Category) values ('$name', '$username', '$password', '$role[0]');");
-      if($submit)
-        echo "Thanks, ".$username." You are now registered as a ".$role.". (and eventually redirected once its implemented";
+    $submit = pg_query(
+        "set search_path = 'foobox'; Insert into User_Account(Name, Username, Password, Category) values ('$name', '$username', '$password', '$role');");
+      if($submit){
+        if($role == 'user'){
+            header("Location: customer/customer.php?page=meal");
+        }
+        if($role == 'chef'){
+            header("Location: Chef/Chef.php?page=meal");
+        }
+        if($role == 'admin'){
+            header("Location: customer/customer.php?page=meal");
+        }
+    }
       else
         echo "could not connect. welp";
 ?>
