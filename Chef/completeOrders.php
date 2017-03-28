3cbec8f9ce6eee028bed93120f072ccf9c671d9c
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-28 18:46:19 -0400

    Completed orders now works

--- Chef/completeOrders.php
+++ Chef/completeOrders.php
@@ -9,16 +9,15 @@
     foreach ($values as $primaryKey){
         //make the meal completed
         $completeOrder = pg_query("update queue set completed=true where ordernumber=$primaryKey;");
-        $mealID = pg_query("select mealsid from queue where ordernumber = $primaryKey;");
-        //Grab all the ingredients for that meal
-        $allIngredients = pg_query("select * from relations where mealsid=$mealID");
-        //Go through all the ingredients and update count of ingredients
-        while($row = pg_fetch_array($allIngredients)){
-            //Calculate the new ingredient count
-            $currentCount = pg_query("select count from ingredient where id=$row[2];");
-            $currentCount = $currentCount - $row[3];
+        $mealID =pg_fetch_array(pg_query("select mealsid from queue where ordernumber = $primaryKey;"));
+
+        $getAllIngredients = pg_query("select ingredientid, numberofingredients from relations where mealid= $mealID[0]");
+        while ($row = pg_fetch_array($getAllIngredients)){
+            //grab current number of ingredients
+            $numIngredients = pg_fetch_array(pg_query("select count from ingredient where id=$row[0];"));
+            $numIngredients = $numIngredients[0] - $row[1];
             //update the database with the new number
-            pg_query("update ingredient set count=$currentCount where id=$row[2]");
+            pg_query("update ingredient set count=$numIngredients where id=$row[0];");
         }
     }
 ?>
