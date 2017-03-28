35d713ef5ef0cdd5f8d73033a610c83b2760e424
Projudah <projudah@gmail.com>
2017-03-28 17:07:08 -0400

    fixed session after merge issue

--- php_includes/session.php
+++ php_includes/session.php
@@ -1,29 +1,6 @@
 <?php
-	//db name: $dbconn4 idk why its named that
-   	include('config.php');
 	  if (!isset($_SESSION['login_user']))
 	   {
 	      session_start();
 	   }
-	$user_check = $_SESSION['login_user'];
-	
-	$ses_sql = pg_query($dbconn4,"set search_path = 'foobox'; SELECT name, username, category FROM user_account WHERE username = '$user_check';");
-	$row = pg_fetch_assoc($ses_sql);
-	$login_session = $row['username'];
-	if(!isset($login_session)){
-		pg_close($dbconn4);
-		header('../index.php');
-	}
-
-   $user_check = $_SESSION['login_user'];
-
-   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
-
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-
-   $login_session = $row['username'];
-
-   if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
-   }
 ?>
