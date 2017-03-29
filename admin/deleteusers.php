4ceb5b71bc6442882992b205ef86380a243c1ea4
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-29 17:34:21 -0400

    Refined Users
    Delete users now works and refined approve users

--- admin/deleteusers.php
+++ admin/deleteusers.php
@@ -1,3 +1,6 @@
+<?php 
+    include('validateAdmin.php')
+?>
 <html>
 	<script language="JavaScript" type="text/javascript" src="../js/select.js"	></script>
     <div style="margin-top:50px;margin-bottom:100px;">
@@ -5,18 +8,18 @@
     	<div class="col-md-5">
         <select style="width:100%;min-height:50%;" id="sbOne" multiple="multiple">
           <?php
-            /*require('../connect.php');
+            require('../connect.php');
             $setPath = pg_query("Set search_path='foobox';");
-            $getUsers = pg_query("  SELECT STATEMENT GOES HERE   ");
+            $getUsers = pg_query("select username, category from user_account where deleted=false;");
             while($row = pg_fetch_array($getUsers)){
-              echo '<option class="op" value="'.$row["id"]. '"">' .$row["name"].'</option>';
-            }*/
+              echo '<option class="op" value="'.$row["username"]. '"">' .$row["username"].' (User Type: '.$row["category"].')</option>';
+            }
           ?>
         </select>
         <button style="width:100%;" onclick="populateOnce('sbOne','sbTwo')"type="button" class="btn btn-default" id="left" value="<">Select User</button>
 		</div>
 		<div class="col-md-5">
-		<form action="orderingTheIngredient.php" method="post">
+		<form action="submitDeleteUsers.php" method="post">
         <select name="list[]" style="width:100%;min-height:50%;" id="sbTwo" multiple="multiple">
         </select>
         <button style="width:100%;" type="submit" class="btn btn-default" id="left" value="<">DELETE</button>
