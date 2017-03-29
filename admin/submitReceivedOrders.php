169c376a49f1b2d0a529bcfbf2af600072d7b896
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-29 18:36:30 -0400

    Received Orders now works

---
+++ admin/submitReceivedOrders.php
@@ -0,0 +1,22 @@
+<?php
+    include ('validateAdmin.php');
+    //grab selected values
+    $values = $_POST['list'];
+    //log into database
+    require('../connect.php');
+    $query = pg_query("set search_path='foobox';");
+
+    //Loop through each selection and adding it to the history
+    foreach ($values as $primaryKey){
+        pg_query("update orders set received=true where id='$primaryKey';");
+        $ingredientInfo = pg_fetch_row(pg_query("select ingredient_id, amount from orders where id='$primaryKey';"));
+        $ingredientID = $ingredientInfo[0];
+        $amount = $ingredientInfo[1];
+        $count = pg_fetch_row(pg_query("select count from ingredient where id=$ingredientID;"));
+        $count = $count[0]+$amount;
+        pg_query("update ingredient set count=$count where id=$ingredientID");
+    }
+
+    $url = "admin.php?page=orders";
+    header("Location: $url");
+?>
