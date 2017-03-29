39bf5748b251d41825e55489f1ed2eec87921bc2
Projudah <projudah@gmail.com>
2017-03-29 16:10:32 -0400

    complete requests notification done

--- Chef/addingIngredients.php
+++ Chef/addingIngredients.php
@@ -12,5 +12,8 @@
         $numOfIngredients = pg_num_rows($findIngredient);
         $ingredientNumber = $numOfIngredients+1;
         $result = pg_query("INSERT INTO Ingredient values ($ingredientNumber, '$name', '$type', $count, $price, $threshold, true);");
+        $url = "chef.php?page=meal";
+        $_SESSION['complete']= 'true';
+        header("Location: $url");
   }
 ?>
