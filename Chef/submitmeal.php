82629a3c15a150a2b93b016a12a7d9927eeae8f5
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-29 16:20:07 -0400

    Submit meal works

--- Chef/submitmeal.php
+++ Chef/submitmeal.php
@@ -1,23 +1,34 @@
 <?php
-include('../php_includes/session.php');
-$meal = $_POST['mealname'];
-$type = $_POST['cuisinename'];
-$price = $_POST['price'];
-$price = (string)$price;
+	//grab all the form fields
+	$meal = $_POST['mealname'];
+	$type = $_POST['cuisinename'];
+	$price = $_POST['price'];
+	$price = (string)$price;
+	$ings = $_POST['list'];
+	//Connect to database
+	require('../connect.php');
+	pg_query("set search_path='foobox';");
+	//get the primary key for the new meal
+	$mealID = pg_fetch_array(pg_query("select count(id) from meals;"));
+	$mealID = $mealID[0] + 1;
+	//add meal to the database
+	pg_query("insert into meals values ($mealID, '$meal','$type', $price, true);");
+	//connect meals to ingredients
+	$listIngredients = explode(',', $ings);
+	$numberRelations = pg_fetch_array(pg_query("select count(id) from relations"));
+	$numberRelations = $numberRelations[0];
+	for ($index = 0; $index < count($listIngredients); $index=$index+2){
+		$numberRelations++;
+		$index1=$index+1;
+		$name = $listIngredients[$index1];
+		$ingredientID = pg_fetch_row(pg_query("select id from ingredient where name='$name';"));
+		$ingredientID = $ingredientID[0];
+		$num = $listIngredients[$index];
+		pg_query("insert into relations values ($numberRelations, $mealID, $ingredientID, $num);");
+		echo $name;
+	}
 
-$ings = $_POST['list'];
-$url = "chef.php?page=meal";
-$_SESSION['complete']= 'true';
-header("Location: $url");
-/*require('../connect.php');
-pg_query("set search_path='foobox';");
-$ctable = pg_query("select count(id) from meals");
-$number= pg_fetch_row($ctable)[0] +10;
-$done = pg_query("
-	insert into meals values($number, '$meal','$type', $price, true);
-	");
-
-//$url = "chef/chef.php?page=meal";
-
-//header("Location: $url");*/
+	$url = "chef.php?page=meal";
+	$_SESSION['complete']= 'true';
+	header("Location: $url");
 ?>
