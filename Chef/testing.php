7a215d43b73c7f659bd543c54980d38967861edf
Projudah <projudah@gmail.com>
2017-03-29 17:53:33 -0400

    queue conquered

--- Chef/testing.php
+++ Chef/testing.php
@@ -1,14 +1,4 @@
-<?php 
-  include ('validateChef.php');
-?>
-
-<html>
-<script language="JavaScript" type="text/javascript" src="../js/select.js"	></script>
-<div style="margin-top:50px;margin-bottom:100px;">
-    <h2> Current Orders</h2>
-    <div class="col-md-5">
-        <select style="width:100%;min-height:50%;" id="sbOne" multiple="multiple">
-            <?php
+<?php
                 require('../connect.php');
 
                 $setPath = pg_query("Set search_path='foobox';");
@@ -16,8 +6,8 @@
                 $ings = pg_query("select id,count from ingredient");
 
                 while($ingrow = pg_fetch_array($ings)){
-                    $getOrders = pg_query("Select * from queue where completed=false;");
-                    //echo "new ingredient ";
+                	$getOrders = pg_query("Select * from queue where completed=false;");
+                	//echo "new ingredient ";
                     $count = $ingrow[1];
                     while($row = pg_fetch_array($getOrders)){
                         $result= pg_query("
@@ -49,18 +39,3 @@
                     }
                 }
 ?>
-        </select>
-        <button style="width:100%;" onclick="populateOnce('sbOne','sbTwo')"type="button" class="btn btn-default" id="left" value="<">Select Order</button>
-    </div>
-    <div class="col-md-5">
-        <form action="completeOrders.php" method="post">
-            <select name="list[]" style="width:100%;min-height:50%;" id="sbTwo" multiple="multiple">
-            </select>
-            <button style="width:100%;" type="submit" class="btn btn-default" id="left" value="<">ORDER(S) COMPLETED</button>
-        </form>
-        <button style="width:100%;"onclick="populateOnce('sbTwo','sbOne')"type="button" class="btn btn-default" id="left" value="<">Remove Selected</button>
-
-    </div>
-
-</div>
-</html>
