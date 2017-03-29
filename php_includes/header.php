fac364bdba34a2062f99dd0612574297875cb10d
Projudah <projudah@gmail.com>
2017-03-29 15:41:16 -0400

    complete info works

--- php_includes/header.php
+++ php_includes/header.php
@@ -1,6 +1,7 @@
 <?php 
 	include('config.php');
 	include('session.php');
+  echo print_r($_SESSION);
 ?>
 <html>
   <head>
@@ -13,7 +14,15 @@
             <div class="navbar-header">
                 <p class="navbar-brand">Welcome, <?php echo $_SESSION['login_name']; ?></p>
             </div>
-            
+            <p id="confirm" class= <?php 
+            if(isset($_SESSION['complete'])){
+              echo "confirm";
+              unset($_SESSION['complete']);
+            }else{
+              echo "gone";
+            }
+
+             ?> >COMPLETE</p>
 
             <ul class="nav navbar-nav navbar-right">
                   <li><a href="../../logout.php">Log out</a></li>
