c96f7810ef2cf0d2e01b77653461a35f0a6fe201
Projudah <projudah@gmail.com>
2017-03-29 11:59:39 -0400

    saving index

--- php_includes/header.php
+++ php_includes/header.php
@@ -4,16 +4,16 @@
 ?>
 <html>
   <head>
-    <link rel="stylesheet" href="../css/style.css">
+      <link rel="stylesheet" type="text/css" href="../css/style.css">
   </head>
 
   <body>
-
     <nav role="navigation" class="navbar navbar-default" style="color:#CACFD6;">
         <div class="container">
             <div class="navbar-header">
-                <p class="navbar-brand">Welcome, <?php echo $_SESSION['login_name']; ?></p>
+                <p class="navbar-brand">Welcome, <?php /*echo $_SESSION['login_name']; */?></p>
             </div>
+            
 
             <ul class="nav navbar-nav navbar-right">
                   <li><a href="../../logout.php">Log out</a></li>
