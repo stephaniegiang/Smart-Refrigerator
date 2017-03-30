2f45a63060d0c9a9c6f18c62ad243584a7f7494a
Projudah <projudah@gmail.com>
2017-03-30 18:03:19 -0400

    help halfay done

--- index.php
+++ index.php
@@ -56,11 +56,12 @@
 
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/login.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
   </head>
-  <body
+  <body style="background: url('img/indexf.jpg');">
     <div class="container">
           <div class="row">
           <div class="col-md-6 col-md-offset-3">
