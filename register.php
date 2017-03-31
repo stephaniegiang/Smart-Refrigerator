829eec64c2d2743d36f8a373c1cf94d89db85412
Stephanie Giang <stephaniegiang1@gmail.com>
2017-03-31 14:10:16 -0400

    Redesigned the not authorized and registered page

--- register.php
+++ register.php
@@ -21,3 +21,31 @@
       else
         echo "could not connect. welp";
 ?>
+
+<html>
+<head>
+     <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/login.css">
+
+  <!-- font for foobox -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body style="background: url('img/indexf.jpg');">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <center><div class="logo">foobox.</div></center>
+          <center><div class="slogan1">You are now registered. Account has to be approved by admin before use.</div></center>
+        </div>
+      </div>
+  </div>
+</body>
+</html>
