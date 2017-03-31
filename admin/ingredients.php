83f73e9d348c7e7a8408b6960e0ea46bf40c5a41
Wes <wes.martin17@gmail.com>
2017-03-31 14:26:55 -0400

    Admin Ingredient Report

---
+++ admin/ingredients.php
@@ -0,0 +1,23 @@
+<html>
+	<h2>Ingredient Report</h2>
+	<div class="col-md-12" id="step3">
+		<table class="table table-striped" style="width:100%;">
+			<tr>
+				<th>Ingredient</th>
+				<th>Quantity</th> 
+			</tr>
+			<tr>
+				<td>First ingredient</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td>Second ingredient</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<td>Chicken</td>
+				<td>Infinite</td>
+			</tr>
+		</table>	
+	</div>
+</html>
