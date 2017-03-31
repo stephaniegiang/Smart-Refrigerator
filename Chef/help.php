47b1696b3bad254c8feda927e6f64ef303f14af7
Projudah <projudah@gmail.com>
2017-03-31 12:09:32 -0400

    queue is a hassle lol fixed

--- Chef/help.php
+++ Chef/help.php
@@ -1,16 +1,40 @@
 <html>
 	<div class="head">
-		<h2>Order Meal</h2>
-		<p class="des">Click on the meal(s) you want to order then click "Select Meal"</p>
-		<p class="des">When you are done selecting meals, click order to place your order</p>
-		<p class="des">The chef would proceed to complete your order in time</p>
-		<p class="des">If there is insufficient ingredients for the meal, it will take longer to be made</p>
+		<h2>Add Meal</h2>
+		<p class="des">Here you can add Meals that can be made with available ingredients</p>
+		<p class="des">Enter al detils required for the new meal</p>
+		<p class="des">If there are missing ingredients, please add the required ingredients in the Add ingredient section</p>
 	</div>
 	<div class="head">
-		<h2>Order Ingredients</h2>
-		<p class="des">Click on the Ingredient(s) you want to order then click "Select Meal"</p>
+		<h2>Delete Meal</h2>
+		<p class="des">Click on the Meal you want to Delete then click "DELETE"</p>
 		<p class="des">When you are done selecting Ingredients, click order to confirm your selection</p>
 		<p class="des">You can proceed to remove Ingredient(s) from the fridge</p>
+	</div>
+	<div class="head">
+		<h2>My Orders</h2>
+		<p class="des">Here you can view your Orders</p>
+		<p class="des">The completed orders and the Queued orders are separate</p>
+	</div>
+	<div class="head">
+		<h2>My Orders</h2>
+		<p class="des">Here you can view your Orders</p>
+		<p class="des">The completed orders and the Queued orders are separate</p>
+	</div>
+	<div class="head">
+		<h2>My Orders</h2>
+		<p class="des">Here you can view your Orders</p>
+		<p class="des">The completed orders and the Queued orders are separate</p>
+	</div>
+	<div class="head">
+		<h2>My Orders</h2>
+		<p class="des">Here you can view your Orders</p>
+		<p class="des">The completed orders and the Queued orders are separate</p>
+	</div>
+	<div class="head">
+		<h2>My Orders</h2>
+		<p class="des">Here you can view your Orders</p>
+		<p class="des">The completed orders and the Queued orders are separate</p>
 	</div>
 	<div class="head">
 		<h2>My Orders</h2>
