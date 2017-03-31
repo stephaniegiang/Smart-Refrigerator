c012a98bfac915e2c90eeba0369b4b767c1e56dc
Projudah <projudah@gmail.com>
2017-03-31 14:50:04 -0400

    help to chef lower case

---
+++ chef/help.php
@@ -0,0 +1,48 @@
+<html>
+	<div class="head">
+		<h2>Add Meal</h2>
+		<p class="des">Here you can add Meals that can be made with available ingredients</p>
+		<p class="des">Enter al detils required for the new meal</p>
+		<p class="des">If there are missing ingredients, please add the required ingredients in the Add ingredient section</p>
+	</div>
+	<div class="head">
+		<h2>Delete Meal</h2>
+		<p class="des">Select the Meal(s) you want to Delete then click "DELETE"</p>
+	</div>
+	<div class="head">
+		<h2>Add Ingredient</h2>
+		<p class="des">Here you can add new ingredients</p>
+		<p class="des">Enter the details required for the new ingredient</p>
+		<p class="des">Click dd Ingredient, and the ingredient will be available</p>
+	</div>
+	<div class="head">
+		<h2>Order Ingredient</h2>
+		<p class="des">Here you can Order exsisting ingredients</p>
+		<p class="des">Select the ingredient you want to order and the amount you need</p>
+		<p class="des">Click the +(plus) button to confirm and multiple ingredients</p>
+		<p class="des">When done click Order ingredients</p>
+		<p class="des">The Admin will then approve your Order</p>
+	</div>
+	<div class="head">
+		<h2>Delete Ingredient</h2>
+		<p class="des">Here you can Delete existing ingredients</p>
+		<p class="des">Select the ingredient(s) you want to delete, then click DELETE</p>
+	</div>
+	<div class="head">
+		<h2>View queue</h2>
+		<p class="des">Here you can Customer Meal Orders</p>
+		<p class="des">Once a meal is completed, please specify which meal are completed and remove from the list</p>
+		<p class="des">Select the completed meal(s) then click Order(s) completed</p>
+	</div>
+	<div class="head">
+		<h2>View Open Orders</h2>
+		<p class="des">Here you can view your Ingredient Orders</p>
+		<p class="des">Aprroved orders shows the orders aproved by an admin but are not yet in</p>
+		<p class="des">Awaiting approval shows the orders that are yet to be approved</p>
+	</div>
+	<div class="head">
+		<h2>Meal Report</h2>
+		<p class="des">Here you can view report on existing meals</p>
+		<p class="des">All available ingredients and for meals are shown here</p>
+	</div>
+</html>
